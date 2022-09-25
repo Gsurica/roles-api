7a8e6760137c271afd5e41b8e2b9eadbdf10db9f
gsurica <eosurica@gmail.com>
2022-09-24 23:10:05 -0300

    Add eslint, editorconfig and prettier

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,7 @@
 export class AppServer {
-    private app: string;
+  private app: string;
 
-    constructor (info: string) {
-        this.app = info ?? 'Olá, Dev!';
-    }
+  constructor(info: string) {
+    this.app = info ?? 'Olá, Dev!';
+  }
 }
