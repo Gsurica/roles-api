d240f95cf30b81e9b2f43c3728408601377a018e
gsurica <eosurica@gmail.com>
2022-09-25 10:32:05 -0300

    create findbyname method

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -6,6 +6,9 @@ const rolesRepository = new RolesRepository();
 
 rolesRouter.post('/', (request: Request, response: Response) => {
   const { name } = request.body;
+  const roleAlreadyExists = rolesRepository.findByName(name);
+  if (roleAlreadyExists)
+    return response.status(400).json({ error: 'Role already exists' });
   const role = rolesRepository.create({ name });
   return response.status(201).json(role);
 });
