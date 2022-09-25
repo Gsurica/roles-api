4c7896595b5b4b2d7c11b225a11386bdb7a5e4d8
gsurica <eosurica@gmail.com>
2022-09-25 18:21:27 -0300

    adding roleId foreignkey

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { createUserTable1664139673515 } from './migrations/1664139673515-createU
 import { Role } from './../../roles/entities/Role';
 import { CreateRolesTable1664122363935 } from './migrations/1664122363935-CreateRolesTable';
 import { DataSource } from 'typeorm';
+import { AddRoleIdToUsersTable1664140244965 } from './migrations/1664140244965-AddRoleIdToUsersTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1664122363935, createUserTable1664139673515],
+  migrations: [
+    CreateRolesTable1664122363935,
+    createUserTable1664139673515,
+    AddRoleIdToUsersTable1664140244965,
+  ],
 });
