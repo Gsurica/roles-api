66bd4eace2fa096fb54a13954d1976c224824e81
gsurica <eosurica@gmail.com>
2022-09-25 10:08:47 -0300

    add role entity

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,17 +1,17 @@
+import { Role } from './../../entities/Role';
 import { Request, Response, Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
 const rolesRouter = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRouter.post('/', (request: Request, response: Response) => {
   const { name } = request.body;
-  const role = {
-    id: uuidv4(),
+  const role = new Role();
+  Object.assign(role, {
     name,
     created_at: new Date(),
-  };
+  });
   roles.push(role);
   return response.status(201).json(role);
 });
