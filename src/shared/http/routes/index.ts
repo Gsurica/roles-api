287dc93f083986d2926c855de62a1f84328f9180
gsurica <eosurica@gmail.com>
2022-09-25 10:01:06 -0300

    add roles routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { rolesRouter } from 'src/roles/http/routes/roles.routes';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Olá, Dev!' });
 });
+
+routes.use('/roles', rolesRouter);
 
 export { routes };
