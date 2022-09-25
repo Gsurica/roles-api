226dcf85473e673cb38e2e405dbaf27945a18e31
gsurica <eosurica@gmail.com>
2022-09-25 12:16:10 -0300

    adding api documentation with swagger

--- src/roles/useCases/createRole/CreateRoleController.ts
+++ src/roles/useCases/createRole/CreateRoleController.ts
@@ -5,7 +5,7 @@ export class CreateRoleController {
   constructor(private createRoleUseCase: CreateRoleUseCase) {}
   handle(request: Request, response: Response): Response {
     const { name } = request.body;
-    const role = this.createRoleUseCase.execute(name);
+    const role = this.createRoleUseCase.execute({ name });
     return response.status(201).json(role);
   }
 }
