cea2141464bc4a9b487f9c174e2c02efafad2f12
gsurica <eosurica@gmail.com>
2022-09-25 18:09:34 -0300

    create migration create-users-table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,4 @@
+import { createUserTable1664139673515 } from './migrations/1664139673515-createUserTable';
 import { Role } from './../../roles/entities/Role';
 import { CreateRolesTable1664122363935 } from './migrations/1664122363935-CreateRolesTable';
 import { DataSource } from 'typeorm';
@@ -6,5 +7,5 @@ export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1664122363935],
+  migrations: [CreateRolesTable1664122363935, createUserTable1664139673515],
 });
